f7430eb44a2646411040ddad59a5bbe380a236ab
Jader Martins <jadermcs94@gmail.com>
2021-11-26 19:28:05 -0300

    update stastistics

--- continet.py
+++ continet.py
@@ -22,9 +22,24 @@ def main():
                 re.findall(pattern, row['REPLICATION SAMPLE SIZE'])
             disease = row['MAPPED_TRAIT']
             dise.add(disease)
+            origins = set()
             for m in match:
-                comm.add(m)
-                g.add_edge(m, disease)
+                for continent, naming in mapping.items():
+                    for name in naming:
+                        if name in m:
+                            origins.add(continent)
+            for continent in origins:
+                comm.add(continent)
+                g.add_edge(continent, disease)
+    # Compute statistics
+    print("Node count")
+    for n in g.degree:
+        if n[0] in mapping:
+            print(n)
+    print("Average path length")
+    print(nx.average_shortest_path_length(g))
+    print("Average clustering")
+    print(nx.average_clustering(g))
     # Plot distribution statistics
     counter = defaultdict(int)
     for d in g.degree():
@@ -39,15 +54,17 @@ def main():
     plt.xlabel('<k> (log)')
     plt.ylabel('$p_k$ (log)')
     plt.scatter(k, pk, label='probability of <k>')
-    plt.savefig("distribution.png", dpi=300, transparent=True)
+    plt.savefig("continent-distribution.png", dpi=300, transparent=True)
     plt.show()
     # Plot bipartite network
     pos = dict()
-    pos.update((n, (1, i*10)) for i, n in enumerate(comm))
+    pos.update((n, (1, i*900)) for i, n in enumerate(comm))
     pos.update((n, (2, i)) for i, n in enumerate(dise))
+    for i, c in enumerate(comm):
+        plt.text(1, i*900+180, c, horizontalalignment='center')
     nx.draw(g, pos=pos, with_labels=False, width=.25, edge_color=(0,0,0,.25))
-    plt.title("Bipartite graph of Communities and Studied Diseases")
-    plt.savefig("bipartite.png", dpi=300, transparent=True)
+    plt.title("Bipartite graph of Continents and Studied Diseases")
+    plt.savefig("continent-bipartite.png", dpi=300, transparent=True)
     plt.show()
 
 if __name__ == "__main__":
